7701746d05b1cc610d86d69dc8f9743d352ddef7
Prateek Nischal <PNischal@walmartlabs.com>
2020-08-24 01:34:49 +0530

    Add github workflow for test

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod qrcode;
 mod totp;
 
 fn main() {
-    let url = qrcode::extract_totp_uri(String::from("/Users/p0n002h/tmp/test.png")).unwrap();
+    let url = qrcode::extract_totp_uri(String::from("./testdata/test.png")).unwrap();
     let spec = totp::TOTPSpec::new(url);
     println!("{}", spec.get_otp());
 }
